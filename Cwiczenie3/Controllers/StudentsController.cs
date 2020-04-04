2d545f62d4796d8d4d1850e9172f9b488a6338b1
unknown <s17518@pjwstk.edu.pl>
2020-04-04 22:39:34 +0200

    praca domowa

---
+++ Cwiczenie3/Controllers/StudentsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cwiczenie3.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudent()
+        {
+            return "Kowalski, Malewski, Andrzejewski";
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetOneStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }else if(id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono");
+        }
+       
+    }
+}
