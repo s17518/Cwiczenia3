f404d335f43d1a4de655bf7d8dac95bdb0baa470
unknown <s17518@pjwstk.edu.pl>
2020-04-05 16:07:09 +0200

    Finished homework

--- Cwiczenie3/Controllers/StudentsController.cs
+++ Cwiczenie3/Controllers/StudentsController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cwiczenie3.DAL;
+using Cwiczenie3.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cwiczenie3.Controllers
@@ -10,11 +12,28 @@ namespace Cwiczenie3.Controllers
     [Route("api/students")]
     public class StudentsController : ControllerBase
     {
-        [HttpGet]
-        public string GetStudent()
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
         {
-            return "Kowalski, Malewski, Andrzejewski";
+            _dbService = dbService;
         }
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            return Ok(_dbService.GetStudents());
+        }
+
+        //private IActionResult Ok(Func<IEnumerable<Student>> getStudents) => throw new NotImplementedException();
+
+        /*
+[HttpGet]
+public string GetStudent()
+{
+   return "Kowalski, Malewski, Andrzejewski";
+}
+*/
+        /*
         [HttpGet("{id}")]
         public IActionResult GetOneStudent(int id)
         {
@@ -27,6 +46,39 @@ namespace Cwiczenie3.Controllers
             }
             return NotFound("Nie znaleziono");
         }
-       
+        */
+
+        /*
+        [HttpGet("{orderBy}")]
+        public string GetStudents(string orderBy)
+        {
+            return $"Kowalski, Malewski, Andrzejewski sortowanie={orderBy}";
+        }
+        */
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            //... add to database
+            //... generating index number
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+        [HttpPut("{idStudent}")]
+        public IActionResult EditStudent(Student student, int idStudent)
+        {
+            //find from database student with given id
+            //edit parameters with student's data
+            //save to database
+            return Ok("edycja zakończona");
+        }
+        [HttpDelete("{idStudent}")]
+        public IActionResult DeleteStudent(int idStudent)
+        {
+            //find from database student with given id
+            //drop record from database
+            return Ok("usuwanie zakończone");
+        }
+
+
     }
 }
---
+++ Cwiczenie3/DAL/IDbService.cs
@@ -0,0 +1,9 @@
+﻿using Cwiczenie3.Models;
+using System.Collections.Generic;
+namespace Cwiczenie3.DAL
+{
+    public interface IDbService
+    {
+        public IEnumerable<Student> GetStudents();
+    }
+}
